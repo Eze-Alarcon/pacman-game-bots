9f5a9cdc01029efa0bfcb4c16dc6cd2a0c32079a
ezequiel <ps3ezequiel@gmail.com>
2023-05-10 17:56:33 +0200

    refactor(bots): remove player, pellets and power up from pacman

--- src/bots.ts
+++ src/bots.ts
@@ -170,8 +170,6 @@ interface InterfaceColitionElements {
 
 const canvas = window.document.getElementById('canvas') as HTMLCanvasElement
 
-const scoreEl = window.document.getElementById('score') as HTMLSpanElement
-
 // canvas context
 const c = canvas.getContext('2d') as CanvasRenderingContext2D
 
@@ -200,82 +198,6 @@ const boundaries: Boundary[] = []
 const pellets: Pellet[] = []
 const powerUps: PowerUp[] = []
 
-const keys = {
-  up: {
-    pressed: false
-  },
-  left: {
-    pressed: false
-  },
-  down: {
-    pressed: false
-  },
-  right: {
-    pressed: false
-  }
-}
-
-let lastKey = ''
-
-let score = 0
-
-let animationID: number
-
-const GAME_KEYS = {
-  UP: 'w',
-  DOWN: 's',
-  RIGHT: 'd',
-  LEFT: 'a'
-}
-
-/* ============= Add event listeners for the keys ============= */
-
-window.addEventListener('keydown', (event) => {
-  event.preventDefault()
-
-  const { key } = event
-
-  switch (key) {
-    case GAME_KEYS.UP:
-      keys.up.pressed = true
-      lastKey = GAME_KEYS.UP
-      break
-    case GAME_KEYS.DOWN:
-      keys.down.pressed = true
-      lastKey = GAME_KEYS.DOWN
-      break
-    case GAME_KEYS.RIGHT:
-      keys.right.pressed = true
-      lastKey = GAME_KEYS.RIGHT
-      break
-    case GAME_KEYS.LEFT:
-      keys.left.pressed = true
-      lastKey = GAME_KEYS.LEFT
-      break
-  }
-})
-
-window.addEventListener('keyup', (event) => {
-  event.preventDefault()
-
-  const { key } = event
-
-  switch (key) {
-    case GAME_KEYS.UP:
-      keys.up.pressed = false
-      break
-    case GAME_KEYS.DOWN:
-      keys.down.pressed = false
-      break
-    case GAME_KEYS.RIGHT:
-      keys.right.pressed = false
-      break
-    case GAME_KEYS.LEFT:
-      keys.left.pressed = false
-      break
-  }
-})
-
 /* ============= Classes ============= */
 
 class Boundary {
@@ -294,60 +216,6 @@ class Boundary {
 
   draw (): void {
     c.drawImage(this.image, this.position.x, this.position.y)
-  }
-}
-
-class Player {
-  public position: InterfacePositionsXY
-  public velocity: InterfacePositionsXY
-  public radius: number
-  public radiands: number
-  public openRate: number
-  public rotation: number
-  static speed: number = 5
-
-  constructor ({ position, velocity, radius = 15 }: InterfacePlayer) {
-    this.position = position
-    this.velocity = velocity
-    this.radius = radius
-    this.radiands = 0.75
-    this.openRate = 0.08
-    this.rotation = 0
-  }
-
-  draw (): void {
-    c.save()
-    c.translate(this.position.x, this.position.y)
-    c.rotate(this.rotation)
-    c.translate(-this.position.x, -this.position.y)
-    // Note D-1
-    c.beginPath()
-    // Note D-2
-    c.arc(
-      this.position.x,
-      this.position.y,
-      this.radius,
-      this.radiands,
-      Math.PI * 2 - this.radiands
-    )
-    // Note D-3
-    c.lineTo(this.position.x, this.position.y)
-    c.fillStyle = 'yellow'
-    c.fill()
-    c.closePath()
-    c.restore()
-  }
-
-  update (): void {
-    this.draw()
-    this.position.x += this.velocity.x
-    this.position.y += this.velocity.y
-
-    // Note D-4
-    if (this.radiands < 0 || this.radiands > 0.75) {
-      this.openRate = -this.openRate
-    }
-    this.radiands += this.openRate
   }
 }
 
@@ -422,17 +290,6 @@ class PowerUp {
 }
 
 /* ============= Functions and stuff ============= */
-
-const player = new Player({
-  position: {
-    x: Boundary.width + (Boundary.width / 2),
-    y: Boundary.height + (Boundary.height / 2)
-  },
-  velocity: {
-    x: 0,
-    y: 0
-  }
-})
 
 const ghosts = [
   new Ghost({
@@ -681,7 +538,7 @@ function circleCollideWithReactangle ({
   // basically it's one end of the circle with the opposite end of the square
 
   // Note A-2
-  const playerBorders = {
+  const circleBorders = {
     top: circle.position.y - circle.radius + circle.velocity.y, // 1
     right: circle.position.x + circle.radius + circle.velocity.x, // 2
     bottom: circle.position.y + circle.radius + circle.velocity.y, // 3
@@ -697,188 +554,22 @@ function circleCollideWithReactangle ({
 
   // Note A-3
 
-  return playerBorders.top <= boundaryBorders.bottom &&
-  playerBorders.right >= boundaryBorders.left &&
-  playerBorders.bottom >= boundaryBorders.top &&
-  playerBorders.left <= boundaryBorders.right
+  return circleBorders.top <= boundaryBorders.bottom &&
+  circleBorders.right >= boundaryBorders.left &&
+  circleBorders.bottom >= boundaryBorders.top &&
+  circleBorders.left <= boundaryBorders.right
 }
 
 function animate (): void {
   // Note F-1
-  animationID = window.requestAnimationFrame(animate)
+  window.requestAnimationFrame(animate)
   // Note G-1
   c.clearRect(0, 0, canvas.width, canvas.height)
 
-  if (keys.up.pressed && lastKey === GAME_KEYS.UP) {
-    // For each edge (boundary) we will compare if that edge is touching our player
-    for (let i = 0; i < boundaries.length; i++) {
-      const boundary = boundaries[i]
-
-      if (
-        circleCollideWithReactangle({
-          circle: {
-            ...player,
-            velocity: {
-              x: 0,
-              y: -Player.speed
-            }
-          },
-          rectangle: boundary
-        })
-      ) {
-        player.velocity.y = 0
-        break
-      } else {
-        player.velocity.y = -Player.speed
-      }
-    }
-  } else if (keys.down.pressed && lastKey === GAME_KEYS.DOWN) {
-    for (let i = 0; i < boundaries.length; i++) {
-      const boundary = boundaries[i]
-
-      if (
-        circleCollideWithReactangle({
-          circle: {
-            ...player,
-            velocity: {
-              x: 0,
-              y: Player.speed
-            }
-          },
-          rectangle: boundary
-        })
-      ) {
-        player.velocity.y = 0
-        break
-      } else {
-        player.velocity.y = Player.speed
-      }
-    }
-  } else if (keys.left.pressed && lastKey === GAME_KEYS.LEFT) {
-    for (let i = 0; i < boundaries.length; i++) {
-      const boundary = boundaries[i]
-
-      if (
-        circleCollideWithReactangle({
-          circle: {
-            ...player,
-            velocity: {
-              x: -Player.speed,
-              y: 0
-            }
-          },
-          rectangle: boundary
-        })
-      ) {
-        player.velocity.x = 0
-        break
-      } else {
-        player.velocity.x = -Player.speed
-      }
-    }
-  } else if (keys.right.pressed && lastKey === GAME_KEYS.RIGHT) {
-    for (let i = 0; i < boundaries.length; i++) {
-      const boundary = boundaries[i]
-
-      if (
-        circleCollideWithReactangle({
-          circle: {
-            ...player,
-            velocity: {
-              x: Player.speed,
-              y: 0
-            }
-          },
-          rectangle: boundary
-        })
-      ) {
-        player.velocity.x = 0
-        break
-      } else {
-        player.velocity.x = Player.speed
-      }
-    }
-  }
-
-  // render pellets
-  // Note h-1
-  for (let i = pellets.length - 1; i >= 0; i--) {
-    const pellet = pellets[i]
-
-    pellet.draw()
-
-    // touch pellets
-    // Note H-2
-    if ((Math.hypot(
-      pellet.position.x - player.position.x,
-      pellet.position.y - player.position.y
-    ) < pellet.radius + player.radius)) {
-      pellets.splice(i, 1)
-      score += 10
-      scoreEl.innerText = `${score}`
-    }
-  }
-
-  // detect collision beteween ghost and player
-  for (let i = ghosts.length - 1; i >= 0; i--) {
-    const ghost = ghosts[i]
-
-    // player lose condition
-    if ((Math.hypot(ghost.position.x - player.position.x,
-      ghost.position.y - player.position.y)) <
-      (ghost.radius + player.radius)) {
-      if (Ghost.scared) {
-        ghosts.splice(i, 1)
-      } else {
-        cancelAnimationFrame(animationID)
-        console.log('GAME OVER (F)')
-      }
-    }
-  }
-
-  // win condition
-
-  if (pellets.length === 0) {
-    console.log('YOU WIN')
-    cancelAnimationFrame(animationID)
-  }
-
-  // render power up
-  for (let i = powerUps.length - 1; i >= 0; i--) {
-    const powerUp = powerUps[i]
-
-    powerUp.draw()
-
-    // touch powerUps
-    if ((Math.hypot(
-      powerUp.position.x - player.position.x,
-      powerUp.position.y - player.position.y
-    ) < powerUp.radius + player.radius)) {
-      powerUps.splice(i, 1)
-
-      // make ghost scared
-      Ghost.scared = true
-
-      // reset property scared
-      setTimeout(() => { Ghost.scared = false }, 3000)
-    }
-  }
-
-  // Stop player if the player hit a wall
+  // render boundaries
   boundaries.forEach((boundary) => {
     boundary.draw()
-
-    if (
-      circleCollideWithReactangle({
-        circle: player,
-        rectangle: boundary
-      })) {
-      player.velocity.y = 0
-      player.velocity.x = 0
-    }
   })
-
-  player.update()
 
   // remder ghost
   ghosts.forEach((ghost) => {
@@ -963,12 +654,6 @@ function animate (): void {
       ghost.prevCollision = []
     }
   })
-
-  // Note D-5
-  if (player.velocity.x > 0) player.rotation = 0
-  else if (player.velocity.x < 0) player.rotation = Math.PI
-  else if (player.velocity.y > 0) player.rotation = Math.PI / 2
-  else if (player.velocity.y < 0) player.rotation = Math.PI * 1.5
 }
 
 animate()
